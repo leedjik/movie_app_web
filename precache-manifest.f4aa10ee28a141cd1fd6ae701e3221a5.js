8a7f47535628a14b064e7335075d9addb12133dd
leedjik <leedjik@naver.com>
2020-05-24 01:56:11 +0900

    Updates

---
+++ precache-manifest.f4aa10ee28a141cd1fd6ae701e3221a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a2b32b1270f668fafb48c495380aa6",
+    "url": "/movie_app_web/index.html"
+  },
+  {
+    "revision": "2c493ee816fc2202cdac",
+    "url": "/movie_app_web/static/css/main.b9b42ea8.chunk.css"
+  },
+  {
+    "revision": "0a86df0b0c8f609f7eb8",
+    "url": "/movie_app_web/static/js/2.3f59e532.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_web/static/js/2.3f59e532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c493ee816fc2202cdac",
+    "url": "/movie_app_web/static/js/main.1ca41416.chunk.js"
+  },
+  {
+    "revision": "eaae76bfb76db295455d",
+    "url": "/movie_app_web/static/js/runtime-main.1b918b79.js"
+  }
+]);
